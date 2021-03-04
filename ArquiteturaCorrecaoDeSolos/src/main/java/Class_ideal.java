7bf4186b0dba220e99f3690ef0b22ec5324ad65a
Leonardo de Souza Mateus <leonardomateus@alunos.utfpr.edu.br>
2021-03-04 09:25:51 -0300

    CREATE CLASS IDEAL AND YOUR ATTRIBUTES

---
+++ ArquiteturaCorrecaoDeSolos/src/main/java/Class_ideal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author leona
+ */
+public class Class_ideal {
+    private int texturadosolo;
+    private double potassio;
+    private double calcio;
+    private double magnesio;
+    private double fosforo;
+    private double enxofre;
+    
+    
+}
