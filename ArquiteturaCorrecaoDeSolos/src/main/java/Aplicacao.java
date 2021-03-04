7ea0ad3f424259d6047e3b80dfcc1975f0880a70
Leonardo de Souza Mateus <leonardomateus@alunos.utfpr.edu.br>
2021-03-04 11:35:59 -0300

    CREATE APLICACAO CLASS
    Esta classe será responsável pela regra de negócio da aplicação e até o presente momento, ela necessita somente da textura do solo para que posteriormente possamos realizar a atribuição dos valores ideais de cada nutriente de acordo com a textura atual do solo.

---
+++ ArquiteturaCorrecaoDeSolos/src/main/java/Aplicacao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author leona
+ */
+public class Aplicacao {
+    private int texturadosolo;
+
+    public int getTexturadosolo() {
+        return texturadosolo;
+    }
+
+    public void setTexturadosolo(int texturadosolo) {
+        this.texturadosolo = texturadosolo;
+    }
+    
+   
+}
+    
+  
