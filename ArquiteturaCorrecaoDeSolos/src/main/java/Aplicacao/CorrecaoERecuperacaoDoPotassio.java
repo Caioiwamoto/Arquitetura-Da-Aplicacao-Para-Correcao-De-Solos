972e8609f20ce55bda6f119334fc942ba9f17ba7
Leonardo de Souza Mateus <leonardomateus@alunos.utfpr.edu.br>
2021-04-20 15:49:56 -0300

    Updated CorrecaoERecuperacaoDoPotassio class
    - Removi o método construtor e os atributos da classe
    - Modifiquei a estrutura do switch
    - Realizei as alteração na classe de teste para manter o funcionamento adequado da mesma

--- ArquiteturaCorrecaoDeSolos/src/main/java/Aplicacao/CorrecaoERecuperacaoDoPotassio.java
+++ ArquiteturaCorrecaoDeSolos/src/main/java/Aplicacao/CorrecaoERecuperacaoDoPotassio.java
@@ -1,40 +1,19 @@
-
 package Aplicacao;
 
-
 public class CorrecaoERecuperacaoDoPotassio {
-    private double quantDePotassioNoSolo;
-    private double quantDeCalcioNoSolo;
-    private double quantDeMagnseioNoSolo;
-    private double quantDeAluminioHidrogenioNoSolo;
-    private double participacaoPercentualDesejadaDoPotassioNaCTC;
-    
-    public CorrecaoERecuperacaoDoPotassio(
-        double quantDePotassioNoSolo, 
-        double quantDeCalcioNoSolo,
-        double quantDeMagnseioNoSolo,
-        double quantDeAluminioHidrogenioNoSolo,
-        double participacaoPercentualDesejadaDoPotassioNaCTC){
-            
-           this.quantDeAluminioHidrogenioNoSolo = quantDeAluminioHidrogenioNoSolo;
-           this.quantDeCalcioNoSolo = quantDeCalcioNoSolo;
-           this.quantDeMagnseioNoSolo = quantDeMagnseioNoSolo;
-           this.quantDePotassioNoSolo = quantDePotassioNoSolo;
-           this.participacaoPercentualDesejadaDoPotassioNaCTC = participacaoPercentualDesejadaDoPotassioNaCTC;
-    }
     
     EquilibrioECorrecaoCTC ECCTC = new EquilibrioECorrecaoCTC(0.15,5.76,1.63,5.35,30.7);
     
-    double participacaoPercentualAtualDoPotassioNaCTCSolo(){
-        if(this.quantDePotassioNoSolo > 0 && ECCTC.calculoCTCCmol() > 0){
-            return this.quantDePotassioNoSolo / ECCTC.calculoCTCCmol()*100;
+    double participacaoPercentualAtualDoPotassioNaCTCSolo(double quantDePotassioNoSolo){
+        if(quantDePotassioNoSolo > 0 && ECCTC.calculoCTCCmol() > 0){
+            return quantDePotassioNoSolo / ECCTC.calculoCTCCmol()*100;
         }
         return 0.0;
     }
     
-    double participacaoPercentualPotassioNaCTCPosCorrecao(){
-        if(this.participacaoPercentualDesejadaDoPotassioNaCTC > 0.001){
-            return this.participacaoPercentualDesejadaDoPotassioNaCTC;
+    double participacaoPercentualPotassioNaCTCPosCorrecao(double participacaoPercentualDesejadaDoPotassioNaCTC){
+        if(participacaoPercentualDesejadaDoPotassioNaCTC > 0.001){
+            return participacaoPercentualDesejadaDoPotassioNaCTC;
         }
         return 0.0;
     }
@@ -46,27 +25,23 @@ public class CorrecaoERecuperacaoDoPotassio {
         return 0.0;
     }
     
-    double calculoDaNescessidadeDePotassioAdiconar(){
-        if (this.quantDePotassioNoSolo > 0 && 
-            participacaoPercentualAtualDoPotassioNaCTCSolo() > this.quantDePotassioNoSolo){
-            
-            return this.quantDePotassioNoSolo * this.participacaoPercentualDesejadaDoPotassioNaCTC / 
-                    participacaoPercentualAtualDoPotassioNaCTCSolo() - this.quantDePotassioNoSolo;
+    double calculoDaNescessidadeDePotassioAdiconar(double quantDePotassioNoSolo, double participacaoPercentualDesejadaDoPotassioNaCTC){
+        if (quantDePotassioNoSolo > 0 && 
+            participacaoPercentualAtualDoPotassioNaCTCSolo(quantDePotassioNoSolo) > quantDePotassioNoSolo){
+    
+            return quantDePotassioNoSolo * participacaoPercentualDesejadaDoPotassioNaCTC / 
+                    participacaoPercentualAtualDoPotassioNaCTCSolo(quantDePotassioNoSolo) - quantDePotassioNoSolo;
         } 
         return 0.0;
     }
     
-    double calculoDaQuantidadeDaFonteAplicar(int fonteDePotassioUsar){
-        if (calculoDaNescessidadeDePotassioAdiconar() > 0.001){
+    double calculoDaQuantidadeDaFonteAplicar(int fonteDePotassioUsar, double quantDePotassioNoSolo, double participacaoPercentualDesejadaDoPotassioNaCTC ){
+        if (calculoDaNescessidadeDePotassioAdiconar(quantDePotassioNoSolo,participacaoPercentualDesejadaDoPotassioNaCTC) > 0.001){
             switch (fonteDePotassioUsar){
-                case 1:
-                    return (calculoDaNescessidadeDePotassioAdiconar()* 39.1 * 10 * 2 * 1.2 * 100/85 * 100/58.0);
-                case 2:
-                    return (calculoDaNescessidadeDePotassioAdiconar()* 39.1* 10 * 2 * 1.2 * 100/85 * 100/52.0);
-                case 3:
-                    return (calculoDaNescessidadeDePotassioAdiconar()* 39.1 * 10 * 2 * 1.2 * 100/85 * 100/22.0);
-                case 4:
-                    return (calculoDaNescessidadeDePotassioAdiconar()* 39.1 * 10 * 2 * 1.2 * 100/85 * 100/44.0);
+                case 1 -> {return (calculoDaNescessidadeDePotassioAdiconar(quantDePotassioNoSolo,participacaoPercentualDesejadaDoPotassioNaCTC)* 39.1 * 10 * 2 * 1.2 * 100/85 * 100/58.0);}
+                case 2 -> {return (calculoDaNescessidadeDePotassioAdiconar(quantDePotassioNoSolo,participacaoPercentualDesejadaDoPotassioNaCTC)* 39.1* 10 * 2 * 1.2 * 100/85 * 100/52.0);}
+                case 3 -> {return (calculoDaNescessidadeDePotassioAdiconar(quantDePotassioNoSolo,participacaoPercentualDesejadaDoPotassioNaCTC)* 39.1 * 10 * 2 * 1.2 * 100/85 * 100/22.0);}
+                case 4 -> {return (calculoDaNescessidadeDePotassioAdiconar(quantDePotassioNoSolo,participacaoPercentualDesejadaDoPotassioNaCTC)* 39.1 * 10 * 2 * 1.2 * 100/85 * 100/44.0);}
             }      
         }
         return 0.0;
@@ -83,15 +58,11 @@ public class CorrecaoERecuperacaoDoPotassio {
         }
         if(fonteDePotassioUsar == 4){
             switch (texturaSolo) {
-                case 1:
-                    return 0.7*calculoDaQuantidadeDaFonteAplicar/1000;
-                case 2:
-                    return 0.5*calculoDaQuantidadeDaFonteAplicar/1000;
-                default:
-                    return 0.0;
+                case 1 ->{return 0.7*calculoDaQuantidadeDaFonteAplicar/1000;}
+                case 2 ->{return 0.5*calculoDaQuantidadeDaFonteAplicar/1000;}
+                default ->{return 0.0;}
             }
         }
         return 0.0;
-
     }
 }
--- ArquiteturaCorrecaoDeSolos/src/test/java/Aplicacao/CorrecaoERecuperacaoDoPotassioTest.java
+++ ArquiteturaCorrecaoDeSolos/src/test/java/Aplicacao/CorrecaoERecuperacaoDoPotassioTest.java
@@ -6,16 +6,17 @@ import static org.junit.Assert.*;
 
 
 public class CorrecaoERecuperacaoDoPotassioTest {
-        CorrecaoERecuperacaoDoPotassio CRPostassio =  new CorrecaoERecuperacaoDoPotassio(0.15,5.76,1.63,5.35,3.0);
+    
+        CorrecaoERecuperacaoDoPotassio CRPostassio =  new CorrecaoERecuperacaoDoPotassio();
         
         @Test
         public void testParticipacaoPercentualAtualDoPotassioNaCTCSolo(){
-            assertEquals(1.1636927851047323, CRPostassio.participacaoPercentualAtualDoPotassioNaCTCSolo(), 0.0);
+            assertEquals(1.1636927851047323, CRPostassio.participacaoPercentualAtualDoPotassioNaCTCSolo(0.15), 0.0);
         }
         
         @Test
         public void testParticipacaoPercentualPotassioNaCTCPosCorrecao(){
-            assertEquals(3.0, CRPostassio.participacaoPercentualPotassioNaCTCPosCorrecao(), 0.0);
+            assertEquals(3.0, CRPostassio.participacaoPercentualPotassioNaCTCPosCorrecao(3.0), 0.0);
         }
         
         @Test
@@ -26,33 +27,33 @@ public class CorrecaoERecuperacaoDoPotassioTest {
         
         @Test
         public void testCalculoDaNescessidadeDePotassioAdiconar(){
-            assertEquals(0.2367, CRPostassio.calculoDaNescessidadeDePotassioAdiconar(), 0);
+            assertEquals(0.2367, CRPostassio.calculoDaNescessidadeDePotassioAdiconar(0.15,3.0), 0);
         }
         
         @Test
         public void testQuantidadeDaFonteAplicar(){
-            assertEquals(450.5462068965517, CRPostassio.calculoDaQuantidadeDaFonteAplicar(1), 0.0);
-            assertEquals(502.5323076923077, CRPostassio.calculoDaQuantidadeDaFonteAplicar(2), 0.0);
-            assertEquals(1187.8036363636363, CRPostassio.calculoDaQuantidadeDaFonteAplicar(3), 0.0);
-            assertEquals(593.9018181818182, CRPostassio.calculoDaQuantidadeDaFonteAplicar(4), 0.0);
+            assertEquals(450.5462068965517, CRPostassio.calculoDaQuantidadeDaFonteAplicar(1,0.15,3.0), 0.0);
+            assertEquals(502.5323076923077, CRPostassio.calculoDaQuantidadeDaFonteAplicar(2,0.15,3.0), 0.0);
+            assertEquals(1187.8036363636363, CRPostassio.calculoDaQuantidadeDaFonteAplicar(3,0.15,3.0), 0.0);
+            assertEquals(593.9018181818182, CRPostassio.calculoDaQuantidadeDaFonteAplicar(4,0.15,3.0), 0.0);
         }
        
         @Test
         public void testCalculoDoCustoDaFonteUtilizarPorHectare(){
             
             assertEquals(1126.3655172413794, CRPostassio.calculoDoCustoDaFonteUtilizarPorHectare(1, 2500.00, 
-                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(1), 1), 0.0);
+                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(1,0.15,3.0), 1), 0.0);
                       
             assertEquals(1256.3307692307694, CRPostassio.calculoDoCustoDaFonteUtilizarPorHectare(2, 2500.00, 
-                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(2), 1), 0.0);
+                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(2,0.15,3.0), 1), 0.0);
             
             assertEquals(2969.509090909091, CRPostassio.calculoDoCustoDaFonteUtilizarPorHectare(3, 2500.00, 
-                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(3), 1), 0.0);
+                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(3,0.15,3.0), 1), 0.0);
             
             assertEquals(0.4157312727272727, CRPostassio.calculoDoCustoDaFonteUtilizarPorHectare(4, 2500.00, 
-                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(4), 1), 0.0);
+                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(4,0.15,3.0), 1), 0.0);
             
             assertEquals(0.2969509090909091, CRPostassio.calculoDoCustoDaFonteUtilizarPorHectare(4, 2500.00, 
-                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(4), 2), 0.0);
+                                                        CRPostassio.calculoDaQuantidadeDaFonteAplicar(4,0.15,3.0), 2), 0.0);
         }
 }
