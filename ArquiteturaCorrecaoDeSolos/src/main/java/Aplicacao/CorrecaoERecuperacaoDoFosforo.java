e98851f88fe814204cd09af1bcd0690f40e12ab3
Leonardo de Souza Mateus <leonardomateus@alunos.utfpr.edu.br>
2021-04-20 14:32:56 -0300

    Updated CorrecaoERecuperacaoDoSolo class
    - Mudei a estrutura do switch
    - Crie uma classe ENUM
    - Diminui o numero de condicionais
    - Removi os atributos da classe
    - Realizei as devidas alteração para o funcionamento corretos do testes

--- ArquiteturaCorrecaoDeSolos/src/main/java/Aplicacao/CorrecaoERecuperacaoDoFosforo.java
+++ ArquiteturaCorrecaoDeSolos/src/main/java/Aplicacao/CorrecaoERecuperacaoDoFosforo.java
@@ -1,105 +1,67 @@
-
 package Aplicacao;
 
-
 public class CorrecaoERecuperacaoDoFosforo {
-    private double teorDeFosforoAtingir;
-    private double quantFosforoNoSolo;
-    private double eficienciaDoFosforo;
-    private int fonteDeFosforoUtilizar;
     
     double calculoDaQuantidadeDaFonteAplicar(
         double teorDeFosforoAtingir, 
         double quantFosforoNoSolo,
         double eficienciaDoFosforo,
-        int fonteDeFosforoUtilizar){
-        
-            this.teorDeFosforoAtingir = teorDeFosforoAtingir;
-            this.quantFosforoNoSolo = quantFosforoNoSolo;
-            this.eficienciaDoFosforo = eficienciaDoFosforo;
-            this.fonteDeFosforoUtilizar = fonteDeFosforoUtilizar;
-      
-            double quantFosforoFaltante = teorDeFosforoAtingir - quantFosforoNoSolo;
-            
-            if(quantFosforoFaltante > 0.01 && eficienciaDoFosforo > 0){
-                switch(fonteDeFosforoUtilizar){
-                    case 1:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/18;
-                    case 2:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/41;
-                    case 3:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/48;
-                    case 4:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/45;
-                    case 5:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/18;
-                    case 6:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/33;
-                    case 7:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/29;
-                    case 8:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/32;
-                    case 9:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/24;
-                    case 10:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/18.5;
-                    case 11:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/52;
-                    case 12:
-                        return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/18;
-                }
-            }
-            return 0.0;
-    }
+        TiposDeFontesDeFosforo fonteDeFosforoUtilizar){
     
-    double calculoDoCalcioKiloPorHectare(int fonteDeFosforoUtilizar, double calculoDaQuantidadeDaFonteAplicar){
-        if(calculoDaQuantidadeDaFonteAplicar > 0){
-             switch(fonteDeFosforoUtilizar){
-                case 1:
-                    return calculoDaQuantidadeDaFonteAplicar*0.28;
-                case 2:
-                    return calculoDaQuantidadeDaFonteAplicar*0.2;
-                case 3:
-                    return calculoDaQuantidadeDaFonteAplicar*0.09;
-                case 4:
-                    return calculoDaQuantidadeDaFonteAplicar*0.16;
-                case 5:
-                    return calculoDaQuantidadeDaFonteAplicar*0.28;
-                case 6:
-                    return calculoDaQuantidadeDaFonteAplicar*0.52;
-                case 7:
-                    return calculoDaQuantidadeDaFonteAplicar*0.52;
-                case 8:
-                    return calculoDaQuantidadeDaFonteAplicar*0.45;
-                case 9:
-                    return calculoDaQuantidadeDaFonteAplicar*0.28;
-                case 10:
-                    return calculoDaQuantidadeDaFonteAplicar*0.44;
-                case 11:
-                    return 0.0;
-                case 12:
-                    return calculoDaQuantidadeDaFonteAplicar*0.18;
+        double quantFosforoFaltante = teorDeFosforoAtingir - quantFosforoNoSolo;
+            
+        if(quantFosforoFaltante > 0.01 && eficienciaDoFosforo > 0){
+            switch(fonteDeFosforoUtilizar){
+                case SUPERFOSFATO_SIMPLES,
+                     YOORIN,
+                     MULTIF_MAGNESIANO -> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/18;}
+                case SUPERFOSFATO_TRIPLO -> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/41;}
+                case MAP -> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/48;}
+                case DAP -> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/45;}
+                case FOSFATO_ARAD -> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/33;}
+                case FOSFATO_GAFSA -> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/29;}
+                case FOSFATO_DAOUI-> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/32;}
+                case FOSFATO_PATOS_MINAS-> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/24;}
+                case ESCORIA_DE_THOMAS -> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/18.5;}
+                case ACIDO_FOSFORICO -> {return ((quantFosforoFaltante*4.58*100)/ (eficienciaDoFosforo/100))/52;}
             }
         }
         return 0.0;
     }
     
-    double calculoDoEnxofreKiloPorHectare(int fonteDeFosforoUtilizar, double calculoDaQuantidadeDaFonteAplicar ){
+    double calculoDoCalcioKiloPorHectare(TiposDeFontesDeFosforo fonteDeFosforoUtilizar, double calculoDaQuantidadeDaFonteAplicar){
         if(calculoDaQuantidadeDaFonteAplicar > 0){
             switch(fonteDeFosforoUtilizar){
-                case 1:
-                    return calculoDaQuantidadeDaFonteAplicar*0.1;
-                case 5:
-                    return calculoDaQuantidadeDaFonteAplicar*0.15;
-                case 12:
-                    return calculoDaQuantidadeDaFonteAplicar*0.11;
+                case SUPERFOSFATO_SIMPLES, 
+                     YOORIN, 
+                     FOSFATO_PATOS_MINAS-> {return calculoDaQuantidadeDaFonteAplicar*0.28;}
+                case SUPERFOSFATO_TRIPLO -> {return calculoDaQuantidadeDaFonteAplicar*0.2;}
+                case MAP -> {return calculoDaQuantidadeDaFonteAplicar*0.09;}
+                case DAP -> {return calculoDaQuantidadeDaFonteAplicar*0.16;}
+                case FOSFATO_ARAD,
+                     FOSFATO_GAFSA -> {return calculoDaQuantidadeDaFonteAplicar*0.52;}
+                case FOSFATO_DAOUI-> {return calculoDaQuantidadeDaFonteAplicar*0.45;}
+                case ESCORIA_DE_THOMAS-> {return calculoDaQuantidadeDaFonteAplicar*0.44;}
+                case ACIDO_FOSFORICO -> {return 0.0;}
+                case MULTIF_MAGNESIANO-> {return calculoDaQuantidadeDaFonteAplicar*0.18;}
+            }
+        }
+        return 0.0;
+    }
+    
+    double calculoDoEnxofreKiloPorHectare(TiposDeFontesDeFosforo fonteDeFosforoUtilizar, double calculoDaQuantidadeDaFonteAplicar ){
+        if(calculoDaQuantidadeDaFonteAplicar > 0){
+            switch(fonteDeFosforoUtilizar){
+                case SUPERFOSFATO_SIMPLES -> {return calculoDaQuantidadeDaFonteAplicar*0.1;}
+                case YOORIN-> {return calculoDaQuantidadeDaFonteAplicar*0.15;}
+                case MULTIF_MAGNESIANO-> {return calculoDaQuantidadeDaFonteAplicar*0.11;}
             }       
         }
         return 0.0;
     }
     
-    double calculoDoCustoDaFonteUtilizarPorHectare(double calculoDaQuantidadeDaFonteAplicar, double valorPorTonelada){
-        if(this.fonteDeFosforoUtilizar >= 1 && this.fonteDeFosforoUtilizar <= 12){
+    double calculoDoCustoDaFonteUtilizarPorHectare(int fonteDeFosforoUtilizar, double calculoDaQuantidadeDaFonteAplicar, double valorPorTonelada){
+        if(fonteDeFosforoUtilizar >= 1 && fonteDeFosforoUtilizar <= 12){
             return (calculoDaQuantidadeDaFonteAplicar/1000) * valorPorTonelada;
         }
         return 0.0;
---
+++ ArquiteturaCorrecaoDeSolos/src/main/java/Aplicacao/TiposDeFontesDeFosforo.java
@@ -0,0 +1,17 @@
+
+package Aplicacao;
+
+public enum TiposDeFontesDeFosforo {
+    SUPERFOSFATO_SIMPLES,
+    SUPERFOSFATO_TRIPLO,
+    MAP,
+    DAP,
+    YOORIN,
+    FOSFATO_ARAD,
+    FOSFATO_GAFSA,
+    FOSFATO_DAOUI,
+    FOSFATO_PATOS_MINAS,
+    ESCORIA_DE_THOMAS,
+    ACIDO_FOSFORICO,
+    MULTIF_MAGNESIANO;
+}
